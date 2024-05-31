385d6f8443047084837fbbea35153c22c10dc424
GWH-A.E <a.esaki@gftd.works>
2024-05-31 15:17:28 +0900

    logout時に / へリダイレクトされるテストを追加

--- test/test.js
+++ test/test.js
@@ -21,7 +21,7 @@ describe('/login', () => {
       .expect(/<a href="\/auth\/github"/)
       .expect(200);
   });
-
+　
   test('ログイン時はユーザー名が表示される', async () => {
     await request(app)
       .get('/login')
